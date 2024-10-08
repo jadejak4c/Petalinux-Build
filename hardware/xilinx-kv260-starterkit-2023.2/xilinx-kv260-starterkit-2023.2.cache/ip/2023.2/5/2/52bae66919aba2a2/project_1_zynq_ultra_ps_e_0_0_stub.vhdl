7177a501460fd9448a9fae47a01ed0143f214341
jadejak4c <krishnapal.jadeja@galaxeye.space>
2024-10-08 23:36:33 +0530

    first commit for 2023

---
+++ hardware/xilinx-kv260-starterkit-2023.2/xilinx-kv260-starterkit-2023.2.cache/ip/2023.2/5/2/52bae66919aba2a2/project_1_zynq_ultra_ps_e_0_0_stub.vhdl
@@ -0,0 +1,35 @@
+-- Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+-- Copyright 2022-2023 Advanced Micro Devices, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2023.2 (lin64) Build 4029153 Fri Oct 13 20:13:54 MDT 2023
+-- Date        : Sat Oct 14 03:50:42 2023
+-- Host        : xcossw01 running 64-bit Ubuntu 18.04.5 LTS
+-- Command     : write_vhdl -force -mode synth_stub -rename_top decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix -prefix
+--               decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix_ project_1_zynq_ultra_ps_e_0_0_stub.vhdl
+-- Design      : project_1_zynq_ultra_ps_e_0_0
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xck26-sfvc784-2LV-c
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix is
+  Port ( 
+    emio_ttc0_wave_o : out STD_LOGIC_VECTOR ( 2 downto 0 );
+    pl_ps_irq0 : in STD_LOGIC_VECTOR ( 0 to 0 );
+    pl_resetn0 : out STD_LOGIC;
+    pl_clk0 : out STD_LOGIC;
+    pl_clk1 : out STD_LOGIC
+  );
+
+end decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix;
+
+architecture stub of decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "emio_ttc0_wave_o[2:0],pl_ps_irq0[0:0],pl_resetn0,pl_clk0,pl_clk1";
+attribute X_CORE_INFO : string;
+attribute X_CORE_INFO of stub : architecture is "zynq_ultra_ps_e_v3_5_1_zynq_ultra_ps_e,Vivado 2023.2";
+begin
+end;
